f39052af6263dccd39f3a8e383b87c3d9f3dd4ca
yarbasha <yarbasha@gmail.com>
2020-05-07 18:08:37 +0300

    Assignment 2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -18,29 +18,29 @@ class Header extends Component {
           <div className="container">
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
-              <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+              <img src="/assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
             </NavbarBrand>
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
                   <NavLink className="nav-link" to="/home">
                     <span className="fa fa-home fa-lg"></span> Home
-                </NavLink>
+                  </NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink className="nav-link" to="/aboutus">
                     <span className="fa fa-info fa-lg"></span> About Us
-                </NavLink>
+                  </NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink className="nav-link" to="/menu">
                     <span className="fa fa-list fa-lg"></span> Menu
-                </NavLink>
+                  </NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink className="nav-link" to="/contactus">
                     <span className="fa fa-address-card fa-lg"></span> Contact Us
-                </NavLink>
+                  </NavLink>
                 </NavItem>
               </Nav>
             </Collapse>
