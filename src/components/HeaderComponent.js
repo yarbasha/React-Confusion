260a11baaded91464401672790fb08049b41da76
yarbasha <yarbasha@gmail.com>
2020-05-08 15:06:15 +0300

    Controlled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -16,10 +16,10 @@ class Header extends Component {
       <>
         <Navbar dark expand="md">
           <div className="container">
-            <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
               <img src="/assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
             </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
